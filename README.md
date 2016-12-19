2806dcbbfba9fbe1c723ff099696f5d85892acee
Kevin <kevin@rocher-dev.com>
2016-12-19 17:48:16 +0100

    v 0.2
    * Use native system to save metas.
    * add edition in the modal view.
    * add field type select.
    * add field type textarea.

--- README.md
+++ README.md
@@ -17,7 +17,10 @@ Put the code below in your theme's functions.php file. Add new fields to your co
     add_filter( 'wpuattachmetas_metas', 'set_wpuattachmetas_metas', 10, 3 );
     function set_wpuattachmetas_metas( $fields ) {
         $fields['wpu_media_price'] = array(
-            'name' => 'Media price'
+            'label' => 'Media price'
+        );
+        $fields['wpu_media_price'] = array(
+            'label' => 'Media price'
         );
         return $fields;
     }
@@ -25,18 +28,28 @@ Put the code below in your theme's functions.php file. Add new fields to your co
 Fields parameters :
 ---
 
-* "name" : String (optional) / Adds a label to the field administration. Default to ID value.
-* "type" : String (optional) / Set a kind of form field. Default to "text".
+* "label" : String (optional) / Adds a label to the field administration. Default to ID value.
+* "helps" : String (optional) / Adds a help string under the field.
+* "input" : String (optional) / Set a kind of form field. Default to "text".
+* "show_in_edit" : Bool (optional) / Show/Hide in edit view.
+* "show_in_modal" : Bool (optional) / Show/Hide in modal view.
+* "required" : Bool (optional) / Field is required.
 
-Fields types :
+Input types :
 ---
 
 * "text" : input type text.
+* "textarea" : input type textarea.
+* "select" : display a select based on the values contained in the parameter "select_values"
+* "html" : display the content of the parameter named "html".
 
 Roadmap :
 ---
 
-- [ ] Edition in the modal view.
-- [ ] Edition in the add medias view.
-- [ ] Field type select.
+- [x] Edition in the modal view.
+- [x] Edition in the add medias view.
+- [x] Field type select.
+- [ ] Field type taxonomy.
+- [ ] Field type post.
 - [ ] Field type checkbox.
+- [ ] User level.
--- wpuattachmetas.php
+++ wpuattachmetas.php
@@ -4,7 +4,7 @@
 Plugin Name: WPU Attachments Metas
 Plugin URI: https://github.com/WordPressUtilities/wpuattachmetas
 Description: Metadatas for Attachments
-Version: 0.1
+Version: 0.2
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -13,13 +13,13 @@ License URI: http://opensource.org/licenses/MIT
 
 class WPUAttachMetas {
 
+    private $pluginkey = 'wpuattach_';
+
     private $metas = array();
 
     public function __construct() {
-        /* Load values */
-        $this->load_metas();
-        /* Display custom fields */
-        add_action('edit_form_after_title', array(&$this, 'display_custom_fields'), 20);
+        /* Display fields */
+        add_filter('attachment_fields_to_edit', array(&$this, 'display_custom_fields'), 10, 2);
         /* Save values */
         add_action('edit_attachment', array(&$this, 'save_custom_fields'), 20);
     }
@@ -27,41 +27,81 @@ class WPUAttachMetas {
     /**
      * Load metadatas and ensure content is correct
      */
-    public function load_metas() {
-        $tmp_metas = apply_filters('wpuattachmetas_metas', $this->metas);
-        foreach ($tmp_metas as $key => $tmp_meta) {
-            if (!isset($tmp_meta['name'])) {
-                $tmp_meta['name'] = ucfirst($key);
+    public function load_metas($post) {
+        $this->metas = array();
+        if (!is_object($post)) {
+            return;
+        }
+        $metas = apply_filters('wpuattachmetas_metas', array());
+        foreach ($metas as $key => $meta) {
+            if (!isset($meta['input'])) {
+                $meta['input'] = 'text';
             }
-            $this->metas[$key] = $tmp_meta;
+            $meta['original_input'] = $meta['input'];
+            $meta['key'] = $this->pluginkey . $key;
+            $field_name = 'attachments[' . $post->ID . '][' . $meta['key'] . ']';
+            $field_id = 'attachments-' . $post->ID . '-' . $meta['key'];
+            $field_idnamehtml = 'id="' . $field_id . '" name="' . $field_name . '" ';
+            $_value = get_post_meta($post->ID, $key, 1);
+            $meta['value'] = $_value ? $_value : '';
+
+            if (!isset($meta['label'])) {
+                $meta['label'] = ucfirst($key);
+            }
+            if (!isset($meta['select_values'])) {
+                $meta['select_values'] = array(__('No'), __('Yes'));
+            }
+            if ($meta['input'] == 'select') {
+                $meta['input'] = 'html';
+                $meta['html'] = '<select ' . $field_idnamehtml . '><option value="" disabled selected style="display:none;">' . __('Select') . '</option>';
+                foreach ($meta['select_values'] as $skey => $var) {
+                    $meta['html'] .= '<option ' . ($meta['value'] == $skey ? 'selected' : '') . ' value="' . $skey . '">' . $var . '</option>';
+                }
+                $meta['html'] .= '</select>';
+            }
+            $this->metas[$key] = $meta;
         }
     }
 
     /**
-     * Display custom fields under attachment edit form
+     * Disply custom fields
      */
-    public function display_custom_fields($post) {
+    public function display_custom_fields($form_fields, $post) {
+        $this->load_metas($post);
         foreach ($this->metas as $key => $meta) {
-            $_id = 'wpuattach_' . $key;
-            $_value = get_post_meta($post->ID, $_id, 1);
-            echo '<p>';
-            echo '<label for="' . $_id . '"><strong>' . $meta['name'] . '</strong></label><br>';
-            echo '<input type="text" class="widefat" name="' . $_id . '" id="wpuattach_' . $key . '" value="' . esc_attr($_value) . '" />';
-            echo '</p>';
+            $form_fields[$meta['key']] = $meta;
         }
+        return $form_fields;
     }
 
     /**
-     * Save custom fields when attachment is saved
+     * Save custom fields when attachment
      */
     public function save_custom_fields($attachment_id) {
+        $tmp_post = get_post($attachment_id);
+        $this->load_metas($tmp_post);
+        $_req = $_REQUEST['attachments'][$attachment_id];
         foreach ($this->metas as $key => $meta) {
-            $_id = 'wpuattach_' . $key;
-            if (isset($_POST[$_id])) {
-                update_post_meta($attachment_id, $_id, esc_html($_POST[$_id]));
+            if (!isset($_req[$meta['key']])) {
+                continue;
             }
+            $old_value = $meta['value'];
+            $new_value = $_req[$meta['key']];
+
+            switch ($meta['original_input']) {
+            case 'select':
+                if (!array_key_exists($new_value, $meta['select_values'])) {
+                    $new_value = $old_value;
+                }
+                break;
+            default:
+
+            }
+
+            update_post_meta($attachment_id, $key, esc_html($new_value));
         }
     }
+
 }
 
 $WPUAttachMetas = new WPUAttachMetas();
